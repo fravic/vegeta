e87904df50290412505112fa2baa8e66592714c0
Fravic <fravicf@gmail.com>
2013-05-05 04:41:54 -0400

    Remove ember.js

--- src/imageView.js
+++ src/imageView.js
@@ -1,14 +1,36 @@
-$(function() {
+var app = app || {};
+
+$(function($) {
     "use strict";
 
-    window.App = window.App ? window.App : Ember.Application.create();
+    app.ImageView = Backbone.View.extend({
+        MAX_DRAG_X: 300,
 
-    App.ImageView = Ember.View.extend({
         templateName: 'image',
 
-        drag: function() {
-            console.log("drag");
+        initialize: function(a) {
+            _.bindAll(this);
+
+            var template = Handlebars.compile($("#tmpl-image").html());
+            $(".app").append(template({image: a.image, id: a.id}));
+
+            $("#img_" + a.id).hammer().on("drag", this.drag);
+        },
+
+        drag: function(evt) {
+            if (!evt.gesture) {
+                return;
+            }
+            this.setDragProgress(evt.gesture.deltaX / this.MAX_DRAG_X);
+            evt.stopPropagation();
             return false;
-        }
+        },
+
+        dragLeave: function() {
+        },
+
+        setDragProgress: function(prog) {
+            prog = Math.min(Math.max(prog, 0), 1);
+        },
     });
 });
